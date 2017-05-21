d1b7020c5899a1f7e000459f5ab7ef4d5007c1ac
Gonzalo Rodríguez-Canosa <grcanosa@gmail.com>
2017-05-21 11:32:39 +0200

    Added changes to dash scanner

--- dash/dash_scanner.py
+++ dash/dash_scanner.py
@@ -3,16 +3,28 @@
 
 from scapy.all import *
 
+import time
+
+class DashScanner():
+    def __init__(self):
+        self.last_check_time = time.time()
+    def notify_hass():
+        tnow = time.time()
+        print("Dash probe received, last time was "+str(tnow-self.last_check_time)+" seconds ago")
+        self.last_check_time = tnow;
+
+    def arp_scan(self,pkt):
+        if pkt.haslayer(ARP):
+            if pkt[ARP].op == 1: #who-has (request)
+                if pkt[ARP].hwsrc == "c4:a3:66:d3:5b:1e":
+                    #print("Probe from DASH")
+                    self.notify_hass()
 
 
-def notify_hass():
-  
-
-def arp_scan(pkt):
-    if pkt.haslayer(ARP):
-        if pkt[ARP].op == 1: #who-has (request)
-            if pkt[ARP].hwsrc == "c4:a3:66:d3:5b:1e":
-              print("Probe from DASH")
 
 
-sniff(prn=arp_scan,filter="arp",store=0,count=0)                      
+
+
+if name == "__main__":
+  dS = DashScanner()
+  sniff(prn=ds.arp_scan,filter="arp",store=0,count=0)
