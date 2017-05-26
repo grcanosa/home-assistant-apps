e2baac5e2d5f1db55051414fe10b709631c0be5d
Gonzalo Rodríguez-Canosa <grcanosa@gmail.com>
2017-05-26 21:46:28 +0200

    Added message send

--- dash/dash_scanner.py
+++ dash/dash_scanner.py
@@ -4,31 +4,48 @@
 from scapy.all import *
 
 import time
+import requests
+import json
+import random
+import sys
 
-class DashScanner():
+
+
+
+class DashScanner(filename=None,password=""):
     def __init__(self):
         self.last_check_time = time.time()
-        self.MAC_dash_fairy = "ac:63:be:d4:7e:3d"
+        self.url='http://192.168.1.98:8123/api/services/notify/grcanosabot'
+        self.headers={'x-ha-access': password, 'content-type': 'application/json'}
+        self.data={'title': 'Fairy te dice:', 'message': 'message'}
+        self.load_from_file(filename)
+        self.sentences = ["Te quiero"]
+
+    def load_from_file(self,filename):
+        with open(filename) as f:
+            for l in f:
+                l = l.strip();
+                self.sentences.append(l)
+
+
     def notify_hass(self):
-        tnow = time.time()
-        print("Dash probe received, last time was "+str(tnow-self.last_check_time)+" seconds ago")
-        self.last_check_time = tnow;
+        self.data["message"] = random.choice(self.sentences)
+        requests.post(self.url,headers=self.headers,json=self.data)
+
 
     def arp_scan(self,pkt):
         if pkt.haslayer(ARP):
             if pkt[ARP].op == 1: #who-has (request)
-                #print(pkt[ARP].hwsrc)
-                if pkt[ARP].hwsrc == self.MAC_dash_fairy:
-                    #print(pkt[ARP])
-                    pkt.show()
+                if pkt[ARP].hwsrc == "c4:a3:66:d3:5b:1e":
                     #print("Probe from DASH")
-                    self.notify_hass()
-
-
-
+                    tnow = time.time()
+                    #print("Dash probe received, last time was "+str(tnow-self.last_check_time)+" seconds ago")
+                    if tnow-self.last_check_time > 9:
+                        self.notify_hass();
+                    self.last_check_time = tnow;
 
 
 
 if __name__ == "__main__":
-  dS = DashScanner()
+  dS = DashScanner("piropos.txt",sys.argv[1])
   sniff(prn=dS.arp_scan,filter="arp",store=0,count=0)
