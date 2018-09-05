56dbf9763f70475d67e03f51f96f5d5c42e5993e
grcanosa <grcanosa@gmail.com>
2018-09-05 17:38:20 +0000

    Fixed timed slider

--- apps/timedslider.py
+++ apps/timedslider.py
@@ -27,9 +27,9 @@ class TimedSlider(hass.Hass):
             self.step_seconds = float(self.step) * self.step_multiplier
             self.handles.append(self.listen_state(self.state_change, self.args["slider"]))
             #Now we add a zero check handler every 5 minutes
-            self.run_every(self.check_5_min,datetime.time(0,0,0),5*60)
+            self.run_every(self.check_5_min,datetime.datetime.now(),5*60)
 
-    def check_5_min(self):
+    def check_5_min(self,extra):
         try:
             if float(self.get_state(self.args["slider"],"state")) == 0.0:
                 self.turn_off(self.args["onoff"])
